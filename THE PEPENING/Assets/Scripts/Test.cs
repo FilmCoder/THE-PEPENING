afc8fb5e37a6ca5ae2119fa44d0c4a7435401176
Filmcoder <dominicnapoleon@Dominics-MacBook-Pro.local>
2018-08-07 16:28:42 -0400

    removed YamlDotNet and added YamlDotNetForUnity, a different version designed for Unity. seems to fix build errors

--- THE PEPENING/Assets/Scripts/Test.cs
+++ THE PEPENING/Assets/Scripts/Test.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.IO;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Samples.Helpers;
+
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 
-        GameObject[] array = new GameObject[1];
-        array[1] = GameObject.Find("Spawner Plane");
+        //GameObject[] array = new GameObject[1];
+        //array[1] = GameObject.Find("Spawner Plane");
 
 
-        SpawnParams spawnParams
-        = new SpawnParams(array);
+        //SpawnParams spawnParams
+        //= new SpawnParams(array);
 
-        SpawnParams spawnParams2
-        = new SpawnParams(spawnPlanes: new[] {GameObject.Find("Spawner Plane"), GameObject.Find("Spawner Plane")});
-
-		
+        //SpawnParams spawnParams2
+        //= new SpawnParams(spawnPlanes: new[] {GameObject.Find("Spawner Plane"), GameObject.Find("Spawner Plane")});
 	}
 	
 	// Update is called once per frame
